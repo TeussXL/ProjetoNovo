94d584ee30884a37d5a43fca8be29c41f1b3c5cf
teussXL <mateus.rodrigo@aluno.ifsp.edu.br>
2025-05-27 17:41:36 -0300

    animação login

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,3 +77,13 @@ document.addEventListener("DOMContentLoaded", function () {
       containerLogin.classList.remove('active')
     }
   })
+
+  //login
+  const wrapper = document.querySelector('.wrapper');
+  const registerLink = document.querySelector('.register-link');
+  const loginLink = document.querySelector('.login-link');
+
+  registerLink.onclick = (e) => {
+    e.preventDefault();
+    wrapper.classList.add('active');
+  }
